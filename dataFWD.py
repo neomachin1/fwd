6a5e11eaf6f2a89a78575321c6bc088e259548e1
neomachin1 <53592626+neomachin1@users.noreply.github.com>
2023-08-08 17:09:38 -0500

    Add files via upload

--- dataFWD.py
+++ dataFWD.py
@@ -4,12 +4,13 @@ Created on Mon Aug  7 09:35:55 2023
 
 @author: ingeniero01
 """
-import binascii
+import math
 
 archivo = open("ovalo.fwd", "r", encoding=("utf-16"))
 nuevo = open("ovala.fwd", "w")
 
 for h in archivo:
+
     if "Carga   " in h:
         h = h.replace("Carga", "Carga   Drop")
     if "kN   " in h:
@@ -17,7 +18,6 @@ for h in archivo:
     if "----" in h:
         h = h.replace(" --- --------", " --- -------- ------")
     if h[0] == "D":
-        print(h)
         if float(h.split()[3]) > 50:
             h = h.replace(h.split()[3], h.split()[3]+"    150")
         elif float(h.split()[3]) > 40:
@@ -25,8 +25,34 @@ for h in archivo:
         elif float(h.split()[3]) > 28:
             h = h.replace(h.split()[3], h.split()[3]+"    130")
     nuevo.write(h)
+    j = h.split()
+    if h[0] == "D":
+        lat = j[-3:-2]
+        lat1 = int(lat[0][:3])
+        lat2 = float(lat[0][3:])
+        latmin = int(math.modf(lat2)[1])
+        latseg = math.modf(lat2)[0]*60
+        print(lat, lat1,  latmin, round(math.modf(lat2)[0],3), "{0:.5f}".format(latseg) )
+        
+        if lat1 > 0:
+            lati = round(lat1 + (lat2/60),5)
+        else:
+            lati = round(lat1 - (lat2/60),5)
+        
+        long = j[-2:-1]
+        long1 = int(long[0][:4])
+        long2 = float(long[0][4:])
+        if long1 > 0:
+            longi= round(long1 + (long2/60),5)
+        else:
+            longi = round(long1 - (long2/60),5)
+        j.append(lati)
+        j.append(longi)
+        #print(j[18:])
+    
+   # print(j)
 
-
+    
 
 
 nuevo.close()
